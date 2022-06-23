b0fa6cafc4027c421b312ea126d1dbef56a4f592
Kevin Kelley <kkelley622@gmail.com>
2022-06-23 18:17:53 -0500

    commit tree 8b60a26e08e3272c79bf9791fdbb648bcd124cfa

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({ tasks, handleDelete}) {
+  const taskList = tasks.map(task => (
+    <Task
+      text={task.text}
+      category={task.category}
+      key={task.text} 
+      handleDelete={handleDelete}
+    />
+  ))
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
